03ed974047cf415f00442cfdf68f89140dd0afdb
Stefan Beck <stefanbeck@itbeck.de>
2025-06-24 11:16:59 +0200

    latest push

--- db-helper-postgres.sh
+++ db-helper-postgres.sh
@@ -21,6 +21,7 @@ PASSWORD=${POSTGRES_PASSWORD}
 
 DEXEC="docker exec -i datastore-postgres"
 MDB="psql -U ${USER} -d ${DBNAME} -c"
+MDB2="psql -U ${USER} -d ${DBNAME} -t -A -X -q -c"
 
 if [ "$1" == "check-connection" ]; then
     ${DEXEC} pg_isready -U ${USER} -d ${DBNAME}
@@ -51,6 +52,50 @@ elif [ "$1" == "select" ]; then
 elif [ "$1" == "dump-table" ]; then
     ${DEXEC} pg_dump -U ${USER} -d ${DBNAME} -t $2 > $3
 
+elif [ "$1" == "extension-create-vector" ]; then
+    ${DEXEC} ${MDB} "
+    CREATE EXTENSION IF NOT EXISTS vector"
+
+elif [ "$1" == "extensions-show" ]; then
+    ${DEXEC} ${MDB} "
+    SELECT * FROM pg_extension"
+
+elif [ "$1" == "leika-str" ]; then
+    ${DEXEC} ${MDB2} "
+    SELECT json_agg(t)
+    FROM (
+        SELECT * FROM leika_codes where description like '%$2%'
+    ) t"
+
+elif [ "$1" == "leika-nr" ]; then
+    ${DEXEC} ${MDB2} "
+    SELECT json_agg(t)
+    FROM (
+        SELECT * FROM leika_codes where leika_code like '%$2%'
+    ) t"
+
+elif [ "$1" == "leika-detail" ]; then
+    ${DEXEC} ${MDB} "
+    SELECT leika_code, description FROM leika_codes where leika_code like '$2%'"
+
+elif [ "$1" == "leika-typedist" ]; then
+    ${DEXEC} ${MDB} "
+    SELECT type, count(type) AS anz FROM leika_codes GROUP BY type ORDER by anz DESC"
+
+elif [ "$1" == "leika-sdg" ]; then
+    ${DEXEC} ${MDB2} "
+    SELECT json_agg(t)
+    FROM (
+        SELECT * FROM leika_codes where sdg_info_area like '%$2%'
+    ) t"
+
+elif [ "$1" == "leika-sdg-dist" ]; then
+    ${DEXEC} ${MDB2} "
+    SELECT json_agg(t)
+    FROM (
+        SELECT sdg_info_area, count(sdg_info_area) AS cnt
+        FROM leika_codes GROUP BY sdg_info_area ORDER by cnt DESC
+    ) t"
 else
     echo "Usage:"
     echo "$0 check-connection              # check connection"
@@ -60,5 +105,13 @@ else
     echo "$0 table-drop name               # drop (delete) the table name"
     echo "$0 select name                   # select some data from name"
     echo "$0 dump-table table file         # dump table to file"
+    echo "$0 extension-create-vector       # create vector extension"
+    echo "$0 extensions-show               # show installed extensions"
+    echo "$0 leika-str str                 # search for leika description"
+    echo "$0 leika-nr str                  # search for leika code"
+    echo "$0 leika-detail str              # search for leika details"
+    echo "$0 leika-typedist                # search for leika type distribution"
+    echo "$0 leika-sdg str                 # search for sdg info"
+    echo "$0 leika-sdg-dist                # search for sdg distribution"
     exit 1
 fi
